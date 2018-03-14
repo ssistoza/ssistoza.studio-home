50d483dd3571108b3925a0896e02544dcaadc985
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-03-14 11:31:13 -0400

    changed back to redirect and added Pathing for Video, Photo Station.

--- spring-boot-server/src/main/java/personal/study/java8study/controller/MainCtrl.java
+++ spring-boot-server/src/main/java/personal/study/java8study/controller/MainCtrl.java
@@ -3,6 +3,7 @@ package personal.study.java8study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,9 +19,14 @@ public class MainCtrl {
         return "index.html";
     }
 
-    @GetMapping("/syno")
-    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
-        return new ModelAndView("forward:https://10.0.0.10:5001", model);
+    /**
+     * Redirects to Synology Login Screen.
+     * @param model
+     * @return ModelAndView : Redirects user to Synology URL.
+     */
+    @GetMapping("/syno/${path}")
+    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model, @PathVariable String path) {
+        return new ModelAndView("forward:https://ssistoza.studio:5001/${path}", model);
     }
 
 }
