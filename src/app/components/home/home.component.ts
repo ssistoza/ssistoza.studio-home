5f09ef3dc64cb52c003da18ed62d040d518c11ab
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-03-15 10:55:21 -0400

    transitioning titles. Need a better Font Choice.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public title = [ 'Welcome', 'Aloha', 'Sawatdee', 'Alola' ];
+  public titles = [ 'Welcome', 'Aloha', 'Sawatdee', 'Alola' ];
+  public title: String = this.titles[0];
+  public transition: boolean = true;
+  private selectedTitle: number = 0;
 
   constructor() { }
 
-  showTitle() { return this.title[0]; }
+  /**
+   * Changes the current title of the Landing Page.
+   *  - This should only be changed once the title has transitioned out.
+   * @return     {<type>}  { description_of_the_return_value }
+   */
+  changeTitle() {
+    if (this.selectedTitle > this.titles.length) {
+      this.selectedTitle = 0;
+    } else {
+      this.selectedTitle++;
+    }
+    this.title = this.titles[this.selectedTitle];
+  }
 
   ngOnInit() {
-  }
+    const timer = Observable.timer(2000, 1500);
+    timer.subscribe(t => {
+      if ( this.transition ) {
+        // ready to transition to next title screen
+        this.transition = false;
+      } else {
+        this.changeTitle();
+        // Scale In Transition Frist.
+        this.transition = true;
+      }
+    });
+  } // ngOnInit()
 }
+
